16b79a4135b5c40e38069ad7a4e4689f27492cde
rbsantos-code <richardbriansantos91@yahoo.com>
2021-11-30 20:44:58 -0800

    create type definition

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+// import the gql taggeed template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql``;
+
+// export the typeDefs
+module.exports = typeDefs;
