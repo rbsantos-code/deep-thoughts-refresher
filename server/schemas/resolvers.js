aaeb87a00e917638b3378add02154542a6179730
rbsantos-code <richardbriansantos91@yahoo.com>
2021-12-06 19:09:13 -0800

    update query in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        thoughts: async () => {
+            return Thought.find().sort({ createdAt: -1 });
         }
     }
 };
