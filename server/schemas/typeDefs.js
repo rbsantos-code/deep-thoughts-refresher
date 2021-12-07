5bb59e650e0ae1890a43a8b77ab31dea0f9796d8
rbsantos-code <richardbriansantos91@yahoo.com>
2021-12-06 19:42:58 -0800

    add reaction types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,10 +10,18 @@ const typeDefs = gql`
         createdAt: String
         username: String
         reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
     }
 
     type Query{
-        thoughts: [Thought]
+        thoughts(username: String!): [Thought]
     }
 `;
 
