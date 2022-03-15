34956ba56277da48e6e4c5e8089cb3e2ad084f23
rbsantos-code <richardbriansantos91@yahoo.com>
2022-03-14 21:45:13 -0700

    create authjs

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -17,11 +21,21 @@ const Login = (props) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+    try {
+      const { data } = await login({
+        variabels: { ...formState }
+      });
+
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // // clear form values
+    // setFormState({
+    //   email: '',
+    //   password: '',
+    // });
   };
 
   return (
@@ -53,6 +67,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login Failed</div>}
           </div>
         </div>
       </div>
