af6d5e498cc965f086011fff83daa11f560c97b4
rbsantos-code <richardbriansantos91@yahoo.com>
2022-03-16 13:57:01 -0700

    work in progress

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+import { LOGIN_USER } from '../utils/mutations.js';
 import Auth from '../utils/auth';
 
 const Login = (props) => {
